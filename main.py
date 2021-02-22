89d6e84c49649941b656188dcf687ebebe654deb
alex-shen1 <shen.alexander@gmail.com>
2021-02-22 12:57:43 -0500

    Bot deletes messages "properly"

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = 'spArk\'s server'
+GUILD = 'alex\'s test server for fun dev times'
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
+
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
@@ -25,7 +27,45 @@ async def on_ready():
 
 @client.event
 async def on_typing(channel, user, when):
-    print(channel, user, when)
+    print(user, "is typing in", channel)
+
+
+messages = {}
+
+
+@client.event
+async def on_message(message):
+    print(message)
+
+    if message.channel.name == 'office-hours-queue':
+        print("this was in the queue!")
+        user_id = message.author.id
+
+        global messages
+        if user_id not in messages.keys():
+            messages[user_id] = [message]
+        else:
+            messages[user_id].append(message)
+        show_messages(user_id)
+
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    print(member, before, after)
+    # print(messages)
+    if before.channel is not None and after.channel is None:
+        user_id = member.id
+        if user_id in messages.keys():
+            for message in messages[user_id]:
+                await message.delete()
+            messages.pop(user_id)
+    # print(before.channel.name, after.channel.name)
+
+
+def show_messages(user_id):
+    for message in messages[user_id]:
+        print(message.content)
+        pass
 
 
 client.run(TOKEN)
