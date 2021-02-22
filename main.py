49a734a85afad8f6c17db75579107665d7eb9255
Alex Shen <shen.alexander@gmail.com>
2021-02-21 21:29:43 -0500

    Experimenting with intents

--- main.py
+++ main.py
@@ -2,33 +2,30 @@ import os
 import discord
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = 'spArk\'s server'
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
 
-# @client.event
-# async def on_ready():
-#     for guild in client.guilds:
-#         if guild.name == GUILD:
-#             break
-#
-#     print(
-#         f'{client.user} is connected to the following guild:\n'
-#         f'{guild.name}(id: {guild.id})'
-#     )
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
-class NewClient(discord.Client):
-    async def on_ready(self):
-        for guild in client.guilds:
-            if guild.name == GUILD:
-                break
-        print(self.users)
-        print(
-            f'{client.user} is connected to the following guild:\n'
-            f'{guild.name}(id: {guild.id})'
-        )
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
-client = NewClient()
+
+@client.event
+async def on_typing(channel, user, when):
+    print(channel, user, when)
+
+
 client.run(TOKEN)
